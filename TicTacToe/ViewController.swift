3526bb908c0816d99f3390f2c6be954089c7f18c
Alexey <demitesh@gmail.com>
2022-06-01 23:02:48 +0200

    Main screen setup, made logo

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var onePlayerButton: UIButton!
+    @IBOutlet var twoPlayersButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buttonsSetUp(button: onePlayerButton)
+        buttonsSetUp(button: twoPlayersButton)
+        
     }
 
-
+    func buttonsSetUp(button: UIButton) {
+        
+        //let borderAlpha : CGFloat = 0.7
+        let cornerRadius : CGFloat = 15.0
+        
+        //onePlayerButton.frame = CGRect(x: 100, y: 100, width: 200, height: 40)
+        button.setTitle("One Player", for: UIControl.State.normal)
+        button.backgroundColor = UIColor.clear
+        button.layer.borderWidth = 1.0
+        //onePlayerButton.layer.borderColor = UIColor(white: 1.0, alpha: borderAlpha).cgColor
+        button.layer.cornerRadius = cornerRadius
+    }
+    
+    
+    
 }
+
 
