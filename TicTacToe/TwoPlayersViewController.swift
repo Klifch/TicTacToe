a929c6dc6c824bcb1fe06783adf82899a77b378e
Alexey <demitesh@gmail.com>
2022-06-03 03:23:11 +0200

    Made idetification of the gestures for the UIImageViews. To fix: constraints for each ImageView, circle.png cutted at the bottom.

--- TicTacToe/TwoPlayersViewController.swift
+++ TicTacToe/TwoPlayersViewController.swift
@@ -26,13 +26,64 @@ class TwoPlayersViewController: UIViewController {
         
         let design = DesignPreset()
         design.buttonsSetUp(button: exitButton)
+        
+        makeTap(on: box1, type: .one)
+        makeTap(on: box2, type: .two)
+        makeTap(on: box3, type: .three)
+        makeTap(on: box4, type: .four)
+        makeTap(on: box5, type: .five)
+        makeTap(on: box6, type: .six)
+        makeTap(on: box7, type: .seven)
+        makeTap(on: box8, type: .eight)
+        makeTap(on: box9, type: .nine)
     }
     
     @IBAction func cancelButtonPressed() {
         dismiss(animated: true)
     }
+    
+    // function to recognize the tap on Images and exact Image() that was tapped
+    func makeTap(on image: UIImageView, type box: Box) {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.boxClicked(_:))) // creating gestureRecognizer
+        tap.name = box.rawValue
+        image.isUserInteractionEnabled = true // user moves are not ignored
+        image.addGestureRecognizer(tap) // connecting gestureRecognizer to the image
+    }
+    
+    // action for the box
+    @objc func boxClicked(_ sender: UITapGestureRecognizer) {
+        let selectedBox = getBox(for: sender.name ?? "")
+        selectedBox.image = UIImage(named: "jesus_killer_alpha")
+    }
+    
+    func getBox(for name: String) -> UIImageView {
+        let box = Box(rawValue: name) ?? .one
+        
+        switch box {
+        case .one:
+            return box1
+        case .two:
+            return box2
+        case .three:
+            return box3
+        case .four:
+            return box4
+        case .five:
+            return box5
+        case .six:
+            return box6
+        case .seven:
+            return box7
+        case .eight:
+            return box8
+        case .nine:
+            return box9
+        }
+    }
+    
 }
 
+// all boxes for game figures
 enum Box: String {
     case one, two, three, four, five, six, seven, eight, nine
 }
